c067419c7171700602177efc4d8fdb01acf7b2fc
Rafael Borges <rafaelborgesdev@gmail.com>
2025-07-23 14:14:12 -0300

    add eslint config

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,6 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- Remove all styling from React props/attributes to disable italics
+vim.cmd("highlight @tag.attribute cterm=NONE gui=NONE")
